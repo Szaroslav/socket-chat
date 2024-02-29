74d863ee1e7f7a7c2a5e8ec17421deecdd0fac06
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-02-29 23:10:13 +0100

    Logger: Add success function (#2)

--- common/logger.h
+++ common/logger.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 void info(const char *message, const char *title);
+void success(const char *message, const char *title);
 void error(const char *message, const char *title, bool use_errno);
 
 #endif // __LOGGER_H__
