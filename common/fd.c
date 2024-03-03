4e701b25f6d0fc74cf33e64591531d829e035ae2
Jakub Szaredko <jakub.szaredko@gmail.com>
2024-03-03 21:39:54 +0100

    File descriptor: Create poll function

--- common/fd.c
+++ common/fd.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 #define FILE_CONTROL_FAILURE -1
+#define POLL_FAILURE         -1
 
 void js_fd_nonblock(int file_descriptor) {
   if (fcntl(file_descriptor, F_SETFL, O_NONBLOCK) == FILE_CONTROL_FAILURE) {
@@ -12,4 +13,12 @@ void js_fd_nonblock(int file_descriptor) {
     exit(FILE_CONTROL_FAILURE);
   }
   success("Non-blocking file descriptor set-up succeeded", SUCCESS_TITLE);
+}
+
+void js_poll(struct pollfd *fd_pool, nfds_t fd_pool_size, int timeout) {
+  if (poll(fd_pool, fd_pool_size, timeout) == POLL_FAILURE) {
+    error("Waiting for file descriptor from the pool failed", ERROR_TITLE, true);
+    exit(POLL_FAILURE);
+  }
+  success("Waiting for file descriptor from the pool succeeded", SUCCESS_TITLE);
 }
--- common/fd.h
+++ common/fd.h
@@ -1,8 +1,12 @@
 #ifndef __FILE_DESCRIPTOR_H__
 #define __FILE_DESCRIPTOR_H__
 
-#include <string.h>
+#define _GNU_SOURCE
+#include <poll.h>
+
+#define INFINITE_TIMEOUT -1
 
 void js_fd_nonblock(int file_descriptor);
+void js_poll(struct pollfd *fd_pool, nfds_t fd_pool_size, int timeout);
 
 #endif // __FILE_DESCRIPTOR_H__
