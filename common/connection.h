6ab8e1e522878cff4eb94406d79aeb9831a6ac89
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-03-04 09:34:50 +0100

    Connection: Add HELLO message (#21)

--- common/connection.h
+++ common/connection.h
@@ -5,14 +5,16 @@
 #include <string.h>
 #include <pthread.h>
 
-#define          CONNECTION_SUCCESS  0
-#define          CONNECTION_FAILURE  -1
-#define          EXIT_MESSAGE        "EXIT"
+#define          CONNECTION_SUCCESS    0
+#define          CONNECTION_FAILURE    -1
+#define          HELLO_MESSAGE         "HELLO"
+extern const int HELLO_MESSAGE_LENGTH;
+#define          EXIT_MESSAGE          "EXIT"
 extern const int EXIT_MESSAGE_LENGTH;
 
 typedef struct connection {
-  bool active;
-  int socket_fd;
+  bool      active;
+  int       socket_fd;
   pthread_t thread;
 } connection;
 
@@ -21,7 +23,8 @@ typedef struct connection_params {
   int socket_fd;
 } connection_params;
 
-void js_init_connection_array (connection *connections, int size);
+bool js_is_hello_message(const char *message);
+void js_init_connection_array(connection *connections, int size);
 int  js_connection(connection *connections, int size, int socket_fd);
 int  js_connection_id(connection *connections, int size);
 void js_prepare_connection_termination(connection *connections, int id);
