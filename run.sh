a6ba03d75743fdd38edeb98f271094071c66981b
Jakub Szaredko <jakub.szaredko@gmail.com>
2024-03-07 00:04:52 +0100

    Create run.sh script

---
+++ run.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+SERVER_NAME="server"
+CLIENT_NAME="client"
+BUILD_DIR="build"
+DEPENDENCIES_DIR="common"
+dependencies=(
+  "${DEPENDENCIES_DIR}/logger.c"
+  "${DEPENDENCIES_DIR}/string.c"
+  "${DEPENDENCIES_DIR}/signal.c"
+  "${DEPENDENCIES_DIR}/fd.c"
+  "${DEPENDENCIES_DIR}/socket.c"
+)
+
+if [[ "$1" == "$SERVER_NAME" ]]; then
+  dependencies+=(
+    "${DEPENDENCIES_DIR}/connection.c"
+    "${DEPENDENCIES_DIR}/thread.c"
+    "${CLIENT_NAME}/${CLIENT_NAME}.c"
+  )
+elif [[ "$1" == "$CLIENT_NAME" ]]; then
+  dependencies+=(
+    "${SERVER_NAME}/${SERVER_NAME}t.c"
+  )
+else
+  exit 1
+fi
+
+mkdir -p $BUILD_DIR                                             \
+  && gcc -Wall -O3 $dependencies "$1/$1.c" -o "${BUILD_DIR}/$1" \
+  && ${BUILD_DIR}/$1
