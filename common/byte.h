af6aa2746ac556e504738fb8cdfd8113e586f0ce
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-03-09 23:19:14 +0100

    Byte: Update (#30)

--- common/byte.h
+++ common/byte.h
@@ -1,2 +1,12 @@
-typedef char byte;
-typedef unsigned char ubyte;
+#ifndef __BYTE_H__
+#define __BYTE_H__
+
+#define BYTE_BITS     8
+#define ONE_BYTE_MASK 0xff
+
+typedef unsigned char byte;
+
+void int_to_bytes(byte *source, int value);
+int bytes_to_int(const byte *source);
+
+#endif // __BYTE_H__
