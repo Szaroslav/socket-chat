0c0bbb381ca50fff1113e3180275e70e595b1f89
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-03-09 17:11:20 +0100

    Socket: Distinction between TCP and UDP, extend module of UDP functions (#26)

--- common/socket.c
+++ common/socket.c
@@ -9,11 +9,25 @@
 int js_socket(int domain, int type, int protocol) {
   const int socket_fd = socket(domain, type, protocol);
   if (socket_fd == SOCKET_FAILURE) {
-      error("Socket creation failed", ERROR_TITLE, true);
-      exit(EXIT_FAILURE);
+    error("Socket creation failed", ERROR_TITLE, true);
+    exit(EXIT_FAILURE);
   }
   success("Socket creation succeeded", SUCCESS_TITLE);
   return socket_fd;
+}
+
+void js_socket_option(
+    int socket_fd,
+    int level,
+    int name,
+    const void *value,
+    socklen_t length
+) {
+  if (setsockopt(socket_fd, level, name, value, length) == SOCKET_FAILURE) {
+    error("Socket option setting failed", ERROR_TITLE, true);
+    exit(SOCKET_FAILURE);
+  }
+  success("Socket option setting succeeded", SUCCESS_TITLE);
 }
 
 int js_socket_read(int socket_fd, char *buffer, int max_bytes_to_read) {
@@ -26,17 +40,53 @@ int js_socket_read(int socket_fd, char *buffer, int max_bytes_to_read) {
   return read_bytes;
 }
 
+int js_socket_receive_from(
+  int socket_fd,
+  char *buffer,
+  int max_bytes_to_receive,
+  sockaddr *source_address,
+  socklen_t *address_length,
+  int flags
+) {
+  const int received_bytes = recvfrom(
+    socket_fd, buffer, max_bytes_to_receive, flags, source_address, address_length);
+  if (received_bytes == SOCKET_RECEIVE_FAILURE) {
+    error("Socket receiving from failed", ERROR_TITLE, true);
+    exit(SOCKET_RECEIVE_FAILURE);
+  }
+  success("Socket receiving from succeeded", SUCCESS_TITLE);
+  return received_bytes;
+}
+
 int js_socket_write(int socket_fd, const char *buffer, int max_bytes_to_write) {
-  const int write_bytes = write(socket_fd, buffer, max_bytes_to_write);
-  if (write_bytes == SOCKET_WRITE_FAILURE) {
+  const int written_bytes = write(socket_fd, buffer, max_bytes_to_write);
+  if (written_bytes == SOCKET_WRITE_FAILURE) {
     error("Socket writing failed", ERROR_TITLE, true);
     exit(SOCKET_WRITE_FAILURE);
   }
   success("Socket writing succeeded", SUCCESS_TITLE);
-  return write_bytes;
+  return written_bytes;
 }
 
-void js_socket_bind(int socket_fd, const struct sockaddr *address, socklen_t address_length) {
+int  js_socket_send_to(
+    int socket_fd,
+    const char *buffer,
+    int max_bytes_to_send,
+    const sockaddr *destination_address,
+    socklen_t address_length,
+    int flags
+) {
+  const int send_bytes = sendto(
+    socket_fd, buffer, max_bytes_to_send, flags, destination_address, address_length);
+  if (send_bytes == SOCKET_SEND_FAILURE) {
+    error("Socket sending to failed", ERROR_TITLE, true);
+    exit(SOCKET_SEND_FAILURE);
+  }
+  success("Socket sending to succeeded", SUCCESS_TITLE);
+  return send_bytes;
+}
+
+void js_socket_bind(int socket_fd, const sockaddr *address, socklen_t address_length) {
   if (bind(socket_fd, address, address_length) != SOCKET_SUCCESS) {
     error("Socket binding failed", ERROR_TITLE, true);
     exit(EXIT_FAILURE);
@@ -52,7 +102,7 @@ void js_socket_listen(int socket_fd, int maximum_pending_connections_count) {
   success("Socket listening succeeded", SUCCESS_TITLE);
 }
 
-int js_socket_accept(int socket_fd, struct sockaddr *address,socklen_t *address_length) {
+int js_socket_accept(int socket_fd, sockaddr *address,socklen_t *address_length) {
   const int connection_fd = accept(socket_fd, address, address_length);
   if (connection_fd < SOCKET_SUCCESS) {
     error("Socket accepting failed", ERROR_TITLE, true);
@@ -64,7 +114,7 @@ int js_socket_accept(int socket_fd, struct sockaddr *address,socklen_t *address_
 
 void js_socket_connect(
     int socket_fd,
-    const struct sockaddr *address,
+    const sockaddr *address,
     socklen_t address_length
 ) {
   if (connect(socket_fd, address, address_length) != SOCKET_SUCCESS) {
--- common/socket.h
+++ common/socket.h
@@ -7,16 +7,45 @@
 #define SOCKET_FAILURE         -1
 #define SOCKET_UNDEFINED       -1
 #define SOCKET_READ_FAILURE    -1
+#define SOCKET_RECEIVE_FAILURE -1
 #define SOCKET_WRITE_FAILURE   -1
+#define SOCKET_SEND_FAILURE    -1
 #define MAX_MESSAGE_SIZE_BYTES 4096
 
+typedef enum socket_type {
+  TCP = SOCK_STREAM,
+  UDP = SOCK_DGRAM,
+} socket_type;
+
+typedef struct sockaddr sockaddr;
+
 int  js_socket(int domain, int type, int protocol);
+void js_socket_option(
+  int socket_fd,
+  int level,
+  int name,
+  const void *value,
+  socklen_t length);
 int  js_socket_read(int socket_fd, char *buffer, int max_bytes_to_read);
+int  js_socket_receive_from(
+  int socket_fd,
+  char *buffer,
+  int max_bytes_to_receive,
+  sockaddr *source_address,
+  socklen_t *address_length,
+  int flags);
 int  js_socket_write(int socket_fd, const char *buffer, int max_bytes_to_write);
-void js_socket_bind(int socket_fd, const struct sockaddr *address, socklen_t address_length);
+int  js_socket_send_to(
+  int socket_fd,
+  const char *buffer,
+  int max_bytes_to_send,
+  const sockaddr *destination_address,
+  socklen_t address_length,
+  int flags);
+void js_socket_bind(int socket_fd, const sockaddr *address, socklen_t address_length);
 void js_socket_listen(int socket_fd, int maximum_pending_connections_count);
-int  js_socket_accept(int socket_fd, struct sockaddr *address, socklen_t *address_length);
-void js_socket_connect(int socket_fd, const struct sockaddr *address, socklen_t address_length);
+int  js_socket_accept(int socket_fd, sockaddr *address, socklen_t *address_length);
+void js_socket_connect(int socket_fd, const sockaddr *address, socklen_t address_length);
 void js_socket_close(int *socket_fd);
 
 #endif // __SOCKET_H__
