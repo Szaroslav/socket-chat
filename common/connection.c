49f646c3747b01d08ce326225147ea8890c5a30d
Jakub Szaredko <jakub.szaredko@gmail.com>
2024-03-09 17:16:39 +0100

    Connection: Distinction between TCP and UDP

--- common/connection.c
+++ common/connection.c
@@ -2,10 +2,12 @@
 #include "logger.h"
 #include "thread.h"
 
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 
 #define   BUFFER_SIZE            64
@@ -14,7 +16,7 @@ const int EXIT_MESSAGE_LENGTH  = strlen(EXIT_MESSAGE);
 
 static char buffer[BUFFER_SIZE];
 
-bool js_is_hello_message(const char *message) {
+bool js_is_hello(const char *message) {
   for (int i = 0; i < HELLO_MESSAGE_LENGTH; i++) {
     if (message[i] != HELLO_MESSAGE[i]) {
       return false;
@@ -23,27 +25,67 @@ bool js_is_hello_message(const char *message) {
   return true;
 }
 
+void js_udp_hello(
+    int socket_fd,
+    int connection_id,
+    const sockaddr *destination_address,
+    socklen_t address_length
+) {
+  sprintf(buffer, "%d%s", connection_id, HELLO_MESSAGE);
+  int sent_bytes = js_socket_send_to(
+    socket_fd,
+    buffer,
+    strlen(buffer) + 1,
+    destination_address,
+    address_length,
+    MSG_DONTWAIT);
+  if (sent_bytes < 0) {
+    error("Sending UDP HELLO message failed", ERROR_TITLE, true);
+    exit(EXIT_FAILURE);
+  }
+  success("Sending UDP HELLO message failed", SUCCESS_TITLE);
+}
+
+int js_connection_id_from_udp_message(const char *message) {
+  char connection_id;
+  sscanf(message, "%hhd", &connection_id);
+  return connection_id;
+}
+
 void js_init_connection_array(connection *connections, int size) {
   memset(connections, 0, size * sizeof(connection));
 }
 
-int js_connection(connection *connections, int size, int socket_fd) {
+int js_tcp_connection(connection *connections, int size, int tcp_socket_fd) {
   const int connection_id = js_connection_id(connections, size);
   if (connection_id == CONNECTION_FAILURE) {
     warn("All connections are currently active", WARNING_TITLE);
     return CONNECTION_FAILURE;
   }
 
-  connections[connection_id].socket_fd = socket_fd;
-  connections[connection_id].active    = true;
-  success("Connection creation succeeded", SUCCESS_TITLE);
+  connections[connection_id].tcp_socket_fd = tcp_socket_fd;
+  connections[connection_id].tcp_active    = true;
+  success("TCP connection creation succeeded", SUCCESS_TITLE);
 
   return connection_id;
 }
 
+void js_udp_connection(
+    connection *connections,
+    int id,
+    const struct sockaddr_in *address
+) {
+  memcpy(
+    &connections[id].udp_client_address,
+    address,
+    sizeof(*address));
+  connections[id].udp_active = true;
+  success("UDP connection creation succeeded", SUCCESS_TITLE);
+}
+
 int js_connection_id(connection *connections, int size) {
   for (int i = 0; i < size; i++) {
-    if (!connections[i].active) {
+    if (!connections[i].tcp_active) {
       return i;
     }
   }
@@ -51,18 +93,18 @@ int js_connection_id(connection *connections, int size) {
 }
 
 void js_prepare_connection_termination(connection *connections, int id) {
-  if (!connections[id].active) {
+  if (!connections[id].tcp_active) {
     warn("Inactive connection termination", WARNING_TITLE);
     return;
   }
 
   sprintf(buffer, "Preparing termination of connection %d", id);
   info(buffer, INFO_TITLE);
-  write(connections[id].socket_fd, EXIT_MESSAGE, EXIT_MESSAGE_LENGTH + 1);
+  js_socket_write(connections[id].tcp_socket_fd, EXIT_MESSAGE, EXIT_MESSAGE_LENGTH + 1);
 }
 
 void js_terminate_connection(connection *connections, int id) {
-  if (!connections[id].active) {
+  if (!connections[id].tcp_active) {
     warn("Inactive connection termination", WARNING_TITLE);
     return;
   }
--- common/connection.h
+++ common/connection.h
@@ -1,9 +1,12 @@
 #ifndef __CONNECTION_H__
 #define __CONNECTION_H__
 
+#include "socket.h"
+
 #include <stdbool.h>
 #include <string.h>
 #include <pthread.h>
+#include <netinet/in.h>
 
 #define          CONNECTION_SUCCESS    0
 #define          CONNECTION_FAILURE    -1
@@ -11,11 +14,14 @@
 extern const int HELLO_MESSAGE_LENGTH;
 #define          EXIT_MESSAGE          "EXIT"
 extern const int EXIT_MESSAGE_LENGTH;
+#define          UDP_HEADER_SIZE_BYTES 1
 
 typedef struct connection {
-  bool      active;
-  int       socket_fd;
-  pthread_t thread;
+  bool               tcp_active;
+  int                tcp_socket_fd;
+  bool               udp_active;
+  struct sockaddr_in udp_client_address;
+  pthread_t          thread;
 } connection;
 
 typedef struct connection_params {
@@ -23,9 +29,20 @@ typedef struct connection_params {
   int socket_fd;
 } connection_params;
 
-bool js_is_hello_message(const char *message);
+bool js_is_hello(const char *message);
+void js_udp_hello(
+  int socket_fd,
+  int connection_id,
+  const sockaddr *destination_address,
+  socklen_t address_length);
+int  js_connection_id_from_udp_message(const char *message);
 void js_init_connection_array(connection *connections, int size);
-int  js_connection(connection *connections, int size, int socket_fd);
+int  js_tcp_connection(connection *connections, int size, int socket_fd);
+void js_udp_connection(
+    connection *connections,
+    int id,
+    const struct sockaddr_in *address
+);
 int  js_connection_id(connection *connections, int size);
 void js_prepare_connection_termination(connection *connections, int id);
 void js_terminate_connection(connection *connections, int id);
